4d6f1b215be59c113456665345373e3705592eb0
bhanusekharp <bhanusekharp@VAD011020721.infics.com>
2018-08-09 18:15:12 +0530

    5th

---
+++ Sample/src/File_Io/Example.java
@@ -0,0 +1,21 @@
+package File_Io;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example {
+	@SafeVarargs
+    private void print(List... names) {  
+        for (List<String> name : names) {  
+            System.out.println(name);  
+        }  
+    }  
+    public static void main(String[] args) {  
+        Example obj = new Example();  
+        List<String> list = new ArrayList<String>();  
+        list.add("Kevin");  
+        list.add("Rick"); 
+        list.add("Negan");
+        obj.print(list);  
+    }      
+}
--- Sample/src/File_Io/Write.java
+++ Sample/src/File_Io/Write.java
@@ -6,45 +6,45 @@ import java.io.IOException;
 
 public class Write {
 	public static void main(String[] args) {
-	      FileOutputStream fos = null;
-	      File file;
-	      String mycontent = "This is my Data which needs" +
-		     " to be written into the file";
-	      try {
-	          //Specify the file path here
-		  file = new File("D://java//myfile.txt");
-		  fos = new FileOutputStream(file);
+		FileOutputStream fos = null;
+		File file;
+		String mycontent = "This is my Data which needs" +
+				" to be written into the file";
+		try {
+			//Specify the file path here
+			file = new File("D://java//myfile.txt");
+			fos = new FileOutputStream(file);
 
-	          /* This logic will check whether the file
-		   * exists or not. If the file is not found
-		   * at the specified location it would create
-		   * a new file*/
-		  if (!file.exists()) {
-		     file.createNewFile();
-		  }
+			/* This logic will check whether the file
+			 * exists or not. If the file is not found
+			 * at the specified location it would create
+			 * a new file*/
+			if (!file.exists()) {
+				file.createNewFile();
+			}
 
-		  /*String content cannot be directly written into
-		   * a file. It needs to be converted into bytes
-		   */
-		  byte[] bytesArray = mycontent.getBytes();
+			/*String content cannot be directly written into
+			 * a file. It needs to be converted into bytes
+			 */
+			byte[] bytesArray = mycontent.getBytes();
 
-		  fos.write(bytesArray);
-		  fos.flush();
-		  System.out.println("File Written Successfully");
-	       } 
-	       catch (IOException ioe) {
-		  ioe.printStackTrace();
-	       } 
-	       finally {
-		  try {
-		     if (fos != null) 
-		     {
-			 fos.close();
-		     }
-	          } 
-		  catch (IOException ioe) {
-		     System.out.println("Error in closing the Stream");
-		  }
-	       }
-	   }
+			fos.write(bytesArray);
+			fos.flush();
+			System.out.println("File Written Successfully");
+		} 
+		catch (IOException ioe) {
+			ioe.printStackTrace();
+		} 
+		finally {
+			try {
+				if (fos != null) 
+				{
+					fos.close();
+				}
+			} 
+			catch (IOException ioe) {
+				System.out.println("Error in closing the Stream");
+			}
+		}
+	}
 }
