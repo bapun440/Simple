503fee8aa5c3cc255a7b24223fdbc28d470dc299
bhanusekharp <bhanusekharp@VAD011020721.infics.com>
2018-08-10 13:02:14 +0530

    10th

--- Sample/src/Coflict1.java
+++ Sample/src/Coflict1.java
@@ -1,4 +1,14 @@
 
-public class Coflict1 {
-	
+class Eampl121 {
+	void method()throws ArithmeticException{  
+		  throw new ArithmeticException("ArithmeticException Occurred");  
+		 }  
+		}  
+		public class Coflict1{  
+		   public static void main(String args[])throws ArithmeticException{
+			   Eampl121 obj=new Eampl121();  
+		     obj.method();  
+		    System.out.println("ram");
+		    System.out.println("End Of Program");  
+		  }  
 }
