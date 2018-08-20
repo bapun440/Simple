520ae1c12edc4071f9832427a6ee4dd7542d435f
bhanusekharp <bhanusekharp@VAD011020721.infics.com>
2018-08-20 19:21:43 +0530

    19:21

---
+++ Sample/src/Strm_ApI.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;  
+
+
+public class Strm_ApI {
+	public static void main(String[] args) {  
+		/*List<Integer> list   
+		= Stream.of(1,2,3,4,5,6,7,8,9,10)  
+		.takeWhile(i -> (i % 2 == 0)).collect(Collectors.toList());
+		System.out.println(list);  
+
+		List<Integer> list1   
+		= Stream.of(2,2,3,4,5,6,7,8,9,10)  
+		.dropWhile(i -> (i % 2 == 0)).collect(Collectors.toList());     
+		System.out.println(list1);  
+
+		List<Integer> list2   
+		= Stream.of(2,2,3,4,5,6,7,8,9,10)  
+		.dropWhile(i -> (i % 2 == 0)).collect(Collectors.toList());     
+		System.out.println(list2);
+		
+*/
+		/*Stream.iterate(1, i -> i <= 10, i -> i*3)  
+		.forEach(System.out::println);  */
+	}  
+}
